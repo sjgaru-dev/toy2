0dd8ffdf648bfefecb6001558a8568f1627a7e2e
kimisadev27 <kimisadev27@gmail.com>
2024-07-29 18:24:29 +0900

    feat: api 연결 코드 추가 (#24)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { auth } from './api';
 import { PATH } from './constants/path';
 import RootLayout from './layouts/Root';
 import HomePage from './pages/Home';
@@ -25,6 +28,13 @@ const router = createBrowserRouter([
   },
 ]);
 
-const App = () => <RouterProvider router={router} />;
+const App = () => {
+  useEffect(() => {
+    (async () => {
+      await auth.authStateReady(); // wait for firebase
+    })();
+  }, []);
+  <RouterProvider router={router} />;
+};
 
 export default App;
