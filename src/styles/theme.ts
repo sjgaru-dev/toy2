3a4c51eeec23b4e529dc60500de5a163263c075c
Hyejin <hyejin.frontend@gmail.com>
2024-07-29 23:58:51 +0900

    feat: 글로벌 스타일 및 테마 설정 (#27)
    - theme.ts : 색상, 폰트 사이즈, 요소 높이 테마 설정
    - GlobalStyle.tsx : 글로벌 스타일 설정 (reset.css, 폰트 설정), export GlobalStyle 해줌
    - emotion.d.ts : emotion 타입 설정
        - theme.js에서 export해준 각각의 타입들을 emotion.d.ts에서 import 하여 Theme의 타입에 넣어줌
        - theme의 속성이 추가 되어도 emotion.d.ts 에 일일이 하드코딩 안해도 됨
    - main.tsx : theme, GlobalStyle 적용하여 App 컴포넌트 감싸기

---
+++ src/styles/theme.ts
@@ -0,0 +1,55 @@
+const colors = {
+  primary: '#2DC384',
+  hoverPrimary: '#46AF83',
+  supportOrange: '#FF8E3D',
+  alertRed: '#E45148',
+  black: '#121212',
+  white: '#ffffff',
+  darkestGray: '#333D4B',
+  darkGray: '#8B95A1',
+  lightGray: '#E3E5EB',
+  lightestGray: '#F2F3F6',
+  hoverLightGray: '#D5D7DD',
+  paleRed: '#F68383',
+  paleOrange: '#FFAB8C',
+  paleYellow: '#FAD781',
+  paleGreen: '#95DB94',
+  paleBlue: '#A2CDFF',
+  palePurple: '#C394FF',
+  bgGray: '#F6F7F9', // 배경색
+};
+
+const fontSizes = {
+  xsmall: '10px',
+  small: '12px',
+  normal: '14px',
+  large: '16px',
+  xlarge: '18px',
+  xxlarge: '20px',
+};
+
+// input, button, toast 등의 높이를 정의
+const heights = {
+  short: '32px',
+  medium: '40px',
+  tall: '50px',
+  xtall: '60px',
+};
+
+export type ColorsTypes = typeof colors;
+export type FontSizeTypes = typeof fontSizes;
+export type HeightsTypes = typeof heights;
+
+interface Theme {
+  colors: ColorsTypes;
+  fontSizes: FontSizeTypes;
+  heights: HeightsTypes;
+}
+// ThemeProvider 적용하기 위해 Theme 타입을 정의
+const theme: Theme = {
+  colors,
+  fontSizes,
+  heights,
+};
+
+export default theme;
