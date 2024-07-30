8846ffafb4531d37f089d964f6b1213e01853fe9
deun <devdeun@gmail.com>
2024-07-30 17:01:20 +0900

    refactor: badgeColors 변수 사용으로 중복 코드 리팩토링 (#16)

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,40 +1,31 @@
+/* eslint-disable no-unused-vars */
 import { css } from '@emotion/react';
 
-import theme from '@/styles/theme';
+import theme, { ColorsTypes } from '@/styles/theme';
+
+type BadgeColorsType = 'red' | 'orange' | 'green' | 'gray';
 
 interface BadgeProps {
   label: string;
-  color: 'red' | 'orange' | 'green' | 'gray';
+  color: BadgeColorsType;
 }
 
-const Badge = ({ label, color }: BadgeProps) => (
-  <span className={`color-${color}`} css={badgeStyle}>
-    {label}
-  </span>
-);
+const badgeColors: { [key in BadgeColorsType]: keyof ColorsTypes } = {
+  red: 'alertRed',
+  orange: 'supportOrange',
+  green: 'primary',
+  gray: 'darkGray',
+};
 
-const badgeStyle = css`
+const Badge = ({ label, color }: BadgeProps) => <span css={badgeStyle(color)}>{label}</span>;
+
+const badgeStyle = (color: BadgeColorsType) => css`
   padding: 4px 8px;
   border-radius: 4px;
   font-size: ${theme.fontSizes.small};
   font-weight: bold;
-
-  &.color-red {
-    color: ${theme.colors.alertRed};
-    background-color: ${theme.colors.alertRed + '1a'};
-  }
-  &.color-orange {
-    color: ${theme.colors.supportOrange};
-    background-color: ${theme.colors.supportOrange + '1a'};
-  }
-  &.color-green {
-    color: ${theme.colors.primary};
-    background-color: ${theme.colors.primary + '1a'};
-  }
-  &.color-gray {
-    color: ${theme.colors.darkGray};
-    background-color: ${theme.colors.darkGray + '1a'};
-  }
+  color: ${theme.colors[badgeColors[color]]};
+  background-color: ${theme.colors[badgeColors[color]] + '1a'};
 `;
 
 export default Badge;
