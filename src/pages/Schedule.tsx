b418ddc7542b387aed38b4adae816b59acc169f9
Hyejin <hyejin.frontend@gmail.com>
2024-08-02 18:37:13 +0900

    feat: 오늘의 일정 컴포넌트 추가 (#54)
    - 달력 페이지에 오늘의 일정을 보여주는 컴포넌트 추가
    - 달력에서 받아온 특정 날짜(기본값:오늘날짜)를 받아 보여주고, 해당 날짜의 일정을 보여줌
    - 해당 날짜의 일정의 총 갯수를 보여줌
    - 일정이 없을 경우 "일정이 없습니다" 라는 문구를 보여줌
    - 일정이 있을 경우 일정의 제목과 시간을 보여줌
        - 시작일과 종료일이 같을 경우 시작시간 - 종료시간으로 표시
        - 시작일과 종료일이 다를 경우
            - 시작일이 현재선택된 날짜일 경우: 시작시간 - 23:59 로 표시
            - 종료일이 현재선택된 날짜일 경우: 00:00 - 종료시간으로 표시
            - 시작일과 종료일이 현재선택된 날짜 사이에 있을 경우: 하루종일로 표시

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { css } from '@emotion/react';
 
 import CalendarComponent from '@/components/Calendar/Calendar';
+import DaySchedule from '@/components/daySchedule/DaySchedule';
 import useFetchSchedule from '@/hooks/useFetchSchedule';
 import theme from '@/styles/theme';
 
@@ -15,6 +16,9 @@ const SchedulePage = () => {
       <h1 className='page-title'>일정</h1>
       <div className='wrapper' css={calendarWrapperStyle}>
         <CalendarComponent events={events} activeDate={activeDate} setActiveDate={setActiveDate} />
+      </div>
+      <div>
+        <DaySchedule date={activeDate.toISOString()} schedules={schedule} />
       </div>
     </>
   );
