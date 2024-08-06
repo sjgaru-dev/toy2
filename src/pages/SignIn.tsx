7b98dea5355aa2977e0bdcdaf96af27744f0c33e
deun <devdeun@gmail.com>
2024-08-06 18:06:07 +0900

    design: 로그인 페이지 스타일 수정 (#139)

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -58,47 +58,60 @@ const SignIn = () => {
 
   return (
     <div css={containerStyle}>
-      <div css={logoStyle}>
-        <img src={logo} alt='STUDIO T' />
-      </div>
-      <form onSubmit={onSubmit}>
-        <Input
-          value={email}
-          name='email'
-          placeholder={TEXT.email.placeholder}
-          onChange={onChangeEmail}
-          isError={isInputError}
-          errorMessage={TEXT.email.regexError}
-        />
+      <div className='signin-container'>
+        <div css={logoStyle}>
+          <img src={logo} alt='STUDIO T' />
+        </div>
+        <form onSubmit={onSubmit}>
+          <Input
+            value={email}
+            name='email'
+            placeholder={TEXT.email.placeholder}
+            onChange={onChangeEmail}
+            isError={isInputError}
+            errorMessage={TEXT.email.regexError}
+          />
 
-        <Input
-          type='password'
-          name='password'
-          value={password}
-          placeholder={TEXT.password.placeholder}
-          onChange={onChangePassword}
-          isError={isInputErrorPwd}
-          errorMessage={TEXT.password.regexError}
-        />
-        {status === 'failed' && <span css={errorStyle}>{TEXT.signin.error}</span>}
-        <Button type='submit' styleType={ableLogin ? 'primary' : 'disabled'}>
-          {status === 'loading' ? <Spinner /> : TEXT.signin.label}
-        </Button>
-      </form>
-      <div css={msgStyle}>
-        {TEXT.common.msg}
-        <br />
-        {TEXT.common.contact}
+          <Input
+            type='password'
+            name='password'
+            value={password}
+            placeholder={TEXT.password.placeholder}
+            onChange={onChangePassword}
+            isError={isInputErrorPwd}
+            errorMessage={TEXT.password.regexError}
+          />
+          {status === 'failed' && <span css={errorStyle}>{TEXT.signin.error}</span>}
+          <Button type='submit' styleType={ableLogin ? 'primary' : 'disabled'}>
+            {status === 'loading' ? <Spinner /> : TEXT.signin.label}
+          </Button>
+        </form>
+        <div css={msgStyle}>
+          {TEXT.common.msg}
+          <br />
+          {TEXT.common.contact}
+        </div>
       </div>
     </div>
   );
 };
 
 const containerStyle = css`
-  width: 90%;
-  height: 100vh;
+  max-width: 500px;
+  min-height: 100vh;
   margin: 0 auto;
-  align-content: center;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  .signin-container {
+    width: 100%;
+    height: 100vh;
+    padding: 40% 16px 0;
+    background-color: ${theme.colors.white};
+  }
+
+  input {
+    height: ${theme.heights.tall};
+  }
 `;
 
 const logoStyle = css`
@@ -111,12 +124,13 @@ const msgStyle = css`
   margin-top: 1rem;
   color: ${theme.colors.darkGray};
   font-size: ${theme.fontSizes.normal};
+  line-height: 140%;
 `;
 
 const errorStyle = css`
   display: block;
   margin: 0.5rem 0;
-  font-size: ${theme.fontSizes.normal};
+  font-size: ${theme.fontSizes.small};
   color: ${theme.colors.alertRed};
 `;
 
