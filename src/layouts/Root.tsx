541cbadc7d32c01fee46413cdbeafbe819ed76b2
kimisadev27 <kimisadev27@gmail.com>
2024-08-06 10:36:27 +0900

    feat: 토스트메세지 커스텀 훅 개발 (#127)

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,16 +1,26 @@
 import { css } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 
+import Toast from '@/components/common/Toast';
 import Navbar from '@/components/layout/Navbar';
+import useToast from '@/hooks/useToast';
 
-const RootLayout = () => (
-  <div css={wrapperStyle}>
-    <Navbar />
-    <main>
-      <Outlet />
-    </main>
-  </div>
-);
+const RootLayout = () => {
+  const { isToastOn, toastMsg, onClose } = useToast();
+
+  return (
+    <div css={wrapperStyle}>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+
+      <Toast isVisible={isToastOn} onClose={onClose}>
+        {toastMsg}
+      </Toast>
+    </div>
+  );
+};
 
 const wrapperStyle = css`
   max-width: 500px;
